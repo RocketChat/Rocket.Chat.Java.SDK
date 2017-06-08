db726a864e49071252623d7038d6f8aca3f53f27
sachin <sachinshinde7676@gmail.com>
2017-06-08 13:40:07 +0530

    Created singleton class middleware with createCallback and processcallback methods

--- src/main/java/io/rocketchat/livechat/middleware/LiveChatMiddleware.java
+++ src/main/java/io/rocketchat/livechat/middleware/LiveChatMiddleware.java
@@ -3,9 +3,27 @@ package io.rocketchat.livechat.middleware;
 /**
  * Created by sachin on 8/6/17.
  */
+
 public class LiveChatMiddleware {
 
     //It will contain ConcurrentArrayList of all callbacks
     //Each new response will trigger each of the callback
 
+    private static LiveChatMiddleware middleware= new LiveChatMiddleware();
+
+    private LiveChatMiddleware(){
+
+    }
+
+    public static LiveChatMiddleware getInstance(){
+        return middleware;
+    }
+
+    public void createcallback(){
+
+    }
+
+    public void processcallback(){
+
+    }
 }
