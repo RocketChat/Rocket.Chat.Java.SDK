e5bc775a7bb9356450c70bc2326f4760bb915a6e
sachin <sachinshinde7676@gmail.com>
2017-06-18 19:43:10 +0530

    created two dummy users on localhost and demo server for history and pagination

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,38 @@
-import io.rocketchat.common.network.ReconnectionStrategy;
 import io.rocketchat.livechat.LiveChatAPI;
+import io.rocketchat.livechat.callback.AuthListener;
 import io.rocketchat.livechat.callback.ConnectListener;
+import io.rocketchat.livechat.model.GuestObject;
 
 /**
  * Created by sachin on 7/6/17.
  */
 
-public class Main implements ConnectListener {
+public class Main implements ConnectListener, AuthListener.LoginListener {
 
     private LiveChatAPI liveChat;
-    private LiveChatAPI.ChatRoom chatRoom;
+
+    public static String userName="guest-18";
+    public static String roomId="u7xcgonkr7sh";
+    public static String userID="rQ2EHbhjryZnqbZxC";
+    public static String visitorToken="707d47ae407b3790465f61d28ee4c63d";
+    public static String authToken="VYIvfsfIdBaOy8hdWLNmzsW0yVsKK4213edmoe52133";
+
 
     public void call(){
-        liveChat=new LiveChatAPI("wss://demo.rocket.chat/websocket");
+        liveChat=new LiveChatAPI("ws://localhost:3000/websocket");
+        liveChat.setReconnectionStrategy(null);
+        liveChat.connect(this);
     }
 
     public static void main(String [] args){
         new Main().call();
     }
 
+
     @Override
     public void onConnect(String sessionID) {
         System.out.println("Connected to server");
+        liveChat.login(authToken,this);
     }
 
     @Override
@@ -34,4 +45,18 @@ public class Main implements ConnectListener {
         System.out.println("Got connect error with the server");
     }
 
+
+    @Override
+    public void onLogin(GuestObject object) {
+        System.out.println("login is successful");
+    }
 }
+
+
+/**
+ * RocketChat server dummy user : {"userName":"guest-3829","roomId":"1hrjr4sruo9q1","userId":"9kAri3uXquAnkMeb4","visitorToken":"-57c7cb8f9c53963712368351705f4d9b","authToken":"qTcmnjIrfQB55bTd9GYhuGOOU63WY0-_afbCe8hyX_r"}
+ */
+
+/**
+ * Localhost dummy user: {"userName":"guest-18","roomId":"u7xcgonkr7sh","userId":"rQ2EHbhjryZnqbZxC","visitorToken":"707d47ae407b3790465f61d28ee4c63d","authToken":"VYIvfsfIdBaOy8hdWLNmzsW0yVsKK4213edmoe52133"}
+ */
