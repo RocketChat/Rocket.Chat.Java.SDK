177bd0b36758b3e90b60cc1ea96f8fe745b0b168
sachin <sachinshinde7676@gmail.com>
2017-07-17 17:32:49 +0530

    added test for closing conversation

--- src/test/java/LiveChatAPI/LiveChatRoomTest/CloseConversationTest.java
+++ src/test/java/LiveChatAPI/LiveChatRoomTest/CloseConversationTest.java
@@ -1,20 +1,51 @@
 package LiveChatAPI.LiveChatRoomTest;
 
+import io.rocketchat.common.data.model.ErrorObject;
+import io.rocketchat.livechat.callback.MessageListener;
+import io.rocketchat.livechat.model.GuestObject;
+import io.rocketchat.livechat.model.MessageObject;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.timeout;
 
 /**
  * Created by sachin on 17/7/17.
  */
 public class CloseConversationTest  extends RoomParent{
 
+    @Mock
+    MessageListener.SubscriptionListener subscriptionListener;
+
+    @Captor
+    ArgumentCaptor <String > roomIdCaptor;
+
+    @Captor
+    ArgumentCaptor <MessageObject> messageObjectArgumentCaptor;
+
     @Before
     public void setup(){
         setUpBefore();
     }
+;
+    @Override
+    public void onLogin(GuestObject object, ErrorObject error) {
+        super.onLogin(object, error);
+        room.sendMessage("Hello there, I'm closing the room");
+        room.subscribeRoom(null,subscriptionListener);
+        room.closeConversation();
+    }
 
     @Test
     public void closeConversationTest(){
-
+        Mockito.verify(subscriptionListener, timeout(8000).atLeastOnce()).onAgentDisconnect(roomIdCaptor.capture(),messageObjectArgumentCaptor.capture());
+        Assert.assertNotNull(roomIdCaptor.getValue());
+        Assert.assertNotNull(messageObjectArgumentCaptor.getValue());
+        System.out.println("Room closed with Id "+ roomIdCaptor.getValue() +" by "+messageObjectArgumentCaptor.getValue().getSender().getUserName());
     }
 }
