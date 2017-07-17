9b3a5d3dd565fc116b6cd6b6519934775360bcdc
sachin <sachinshinde7676@gmail.com>
2017-07-17 14:08:47 +0530

    added test suite for chat room

---
+++ src/test/java/LiveChatAPI/LiveChatRoomTestSuite.java
@@ -0,0 +1,16 @@
+package LiveChatAPI;
+
+/**
+ * Created by sachin on 17/7/17.
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({})
+
+public class LiveChatRoomTestSuite {
+
+}
