b750d3ab83fe237ef3bf7b38d4978abbc43ac890
sachin <sachinshinde7676@gmail.com>
2017-07-17 18:11:04 +0530

    created test suite for LiveChatRoom

--- src/test/java/LiveChatAPI/LiveChatRoomTestSuite.java
+++ src/test/java/LiveChatAPI/LiveChatRoomTestSuite.java
@@ -4,12 +4,20 @@ package LiveChatAPI;
  * Created by sachin on 17/7/17.
  */
 
+import LiveChatAPI.LiveChatRoomTest.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 
-@Suite.SuiteClasses({})
+@Suite.SuiteClasses({
+        LoginTest.class,
+        GetAgentDataTest.class,
+        SubscribeTest.class,
+        GetChatHistoryTest.class,
+        SendMessageTest.class,
+        CloseConversationTest.class
+})
 
 public class LiveChatRoomTestSuite {
 
