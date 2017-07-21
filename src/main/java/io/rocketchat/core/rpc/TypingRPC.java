5f24792e26e268e6444b628e3e67b6f868485389
sachin <sachinshinde7676@gmail.com>
2017-07-21 20:04:43 +0530

    added RPC for sending typing event

---
+++ src/main/java/io/rocketchat/core/rpc/TypingRPC.java
@@ -0,0 +1,16 @@
+package io.rocketchat.core.rpc;
+
+import io.rocketchat.common.data.rpc.RPC;
+
+/**
+ * Created by sachin on 21/7/17.
+ */
+public class TypingRPC extends RPC {
+
+    public static String SENDTYPING="stream-notify-room";
+
+    public static String sendTyping(int integer, String room_id, Boolean istyping, String username){
+
+        return getRemoteMethodObject(integer,SENDTYPING,room_id+"/typing",username,istyping).toString();
+    }
+}
