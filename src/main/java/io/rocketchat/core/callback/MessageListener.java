d1a23b971dc9304cc655140c418b9acd72401d4f
sachin <sachinshinde7676@gmail.com>
2017-07-22 11:24:47 +0530

    added MessageListener class in the core

--- src/main/java/io/rocketchat/core/callback/MessageListener.java
+++ src/main/java/io/rocketchat/core/callback/MessageListener.java
@@ -1,8 +1,19 @@
 package io.rocketchat.core.callback;
 
+import io.rocketchat.common.data.model.ErrorObject;
+import io.rocketchat.common.listener.Listener;
+import io.rocketchat.core.model.RocketChatMessage;
+
 /**
  * Created by sachin on 22/7/17.
  */
-public class MessageListener {
 
+public class MessageListener {
+    public interface SubscriptionListener extends Listener {
+        void onMessage(String roomId, RocketChatMessage message);
+    }
+
+    public interface MessageAckListener extends Listener {
+        void onMessageAck(RocketChatMessage message, ErrorObject error);
+    }
 }
