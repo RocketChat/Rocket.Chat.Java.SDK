1188d688c62103e63b8a5bccdab4adb37fba6e98
sachin <sachinshinde7676@gmail.com>
2017-07-29 18:04:53 +0530

    added todo code for SubscriptionListener inside LiveChat

--- src/main/java/io/rocketchat/livechat/callback/SubscribeListener.java
+++ src/main/java/io/rocketchat/livechat/callback/SubscribeListener.java
@@ -6,6 +6,7 @@ import io.rocketchat.livechat.middleware.LiveChatStreamMiddleware;
 /**
  * Created by sachin on 12/6/17.
  */
+// TODO: 29/7/17 Future change to make it more generalized, removing type from the method and adding Boolean success and String subId to the code
 public interface SubscribeListener extends Listener{
     /**
      *
