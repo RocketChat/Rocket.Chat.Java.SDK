9c448c6bdf2e9c5b63b6e7c3354c63df54d040be
sachin <sachinshinde7676@gmail.com>
2017-08-03 01:20:50 +0530

    added test for logging out from server

--- src/test/java/RocketChatAPI/RocketChatTest/LogoutTest.java
+++ src/test/java/RocketChatAPI/RocketChatTest/LogoutTest.java
@@ -2,11 +2,17 @@ package RocketChatAPI.RocketChatTest;
 
 import RocketChatAPI.RocketChatTest.ChatParent.RocketChatParent;
 import io.rocketchat.common.data.model.ErrorObject;
+import io.rocketchat.common.listener.SimpleListener;
 import io.rocketchat.core.model.TokenObject;
-import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.timeout;
 
 /**
  * Created by sachin on 3/8/17.
@@ -15,6 +21,13 @@ public class LogoutTest extends RocketChatParent{
 
     String username="testuserrocks";
     String password="testuserrocks";
+
+
+    @Mock
+    SimpleListener listener;
+
+    @Captor
+    ArgumentCaptor<Boolean> successArgumentCaptor;
 
     @Captor
     ArgumentCaptor<ErrorObject> errorArgumentCaptor;
@@ -32,11 +45,14 @@ public class LogoutTest extends RocketChatParent{
 
     @Override
     public void onLogin(TokenObject token, ErrorObject error) {
-
+        api.logout(listener);
     }
 
-    @After
-    public void logout(){
-        api.logout(null);
+    @Test
+    public void logoutTest(){
+        Mockito.verify(listener, timeout(12000).atLeastOnce()).callback(successArgumentCaptor.capture(),errorArgumentCaptor.capture());
+        Assert.assertNotNull(successArgumentCaptor.getValue());
+        Assert.assertNull(errorArgumentCaptor.getValue());
     }
+
 }
