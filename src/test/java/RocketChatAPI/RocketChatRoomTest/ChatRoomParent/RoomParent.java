39c7eb1cad84cf80fbbddd0d5d84462765813542
sachin <sachinshinde7676@gmail.com>
2017-08-02 23:34:41 +0530

    added connect test for rocketChatAPI

---
+++ src/test/java/RocketChatAPI/RocketChatRoomTest/ChatRoomParent/RoomParent.java
@@ -0,0 +1,7 @@
+package RocketChatAPI.RocketChatRoomTest.ChatRoomParent;
+
+/**
+ * Created by sachin on 2/8/17.
+ */
+public class RoomParent {
+}
---
+++ src/test/java/RocketChatAPI/RocketChatTest/ChatParent/RocketChatParent.java
@@ -0,0 +1,26 @@
+package RocketChatAPI.RocketChatTest.ChatParent;
+
+import io.rocketchat.core.RocketChatAPI;
+import io.rocketchat.core.adapter.CoreAdapter;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Created by sachin on 2/8/17.
+ */
+public class RocketChatParent extends CoreAdapter{
+
+    private static String serverurl="wss://demo.rocket.chat/websocket";
+
+    public RocketChatAPI api;
+
+
+    public void setUpBefore(Boolean connect){
+        MockitoAnnotations.initMocks( this );
+        System.out.println("before got called");
+        api= new RocketChatAPI(serverurl);
+        api.setReconnectionStrategy(null);
+        if (connect) {
+            api.connect(this);
+        }
+    }
+}
