9157c08f0bd8416219a99d1a80d89f385e95b525
sachin <sachinshinde7676@gmail.com>
2017-08-03 01:18:18 +0530

    added test for getting custom emoji from server

--- src/test/java/RocketChatAPI/RocketChatTest/ListCustomEmojiTest.java
+++ src/test/java/RocketChatAPI/RocketChatTest/ListCustomEmojiTest.java
@@ -2,11 +2,21 @@ package RocketChatAPI.RocketChatTest;
 
 import RocketChatAPI.RocketChatTest.ChatParent.RocketChatParent;
 import io.rocketchat.common.data.model.ErrorObject;
+import io.rocketchat.core.callback.EmojiListener;
+import io.rocketchat.core.model.Emoji;
 import io.rocketchat.core.model.TokenObject;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.timeout;
 
 /**
  * Created by sachin on 3/8/17.
@@ -16,6 +26,11 @@ public class ListCustomEmojiTest extends RocketChatParent{
     String username="testuserrocks";
     String password="testuserrocks";
 
+    @Mock
+    EmojiListener listener;
+
+    @Captor
+    ArgumentCaptor <ArrayList <Emoji >> listArgumentCaptor;
     @Captor
     ArgumentCaptor<ErrorObject> errorArgumentCaptor;
 
@@ -32,7 +47,15 @@ public class ListCustomEmojiTest extends RocketChatParent{
 
     @Override
     public void onLogin(TokenObject token, ErrorObject error) {
+        api.listCustomEmoji(listener);
+    }
 
+    @Test
+    public void listCustomEmojiTest(){
+        Mockito.verify(listener, timeout(12000).atLeastOnce()).onListCustomEmoji(listArgumentCaptor.capture(),errorArgumentCaptor.capture());
+        Assert.assertNotNull(listArgumentCaptor.getValue());
+        Assert.assertNull(errorArgumentCaptor.getValue());
+        Assert.assertTrue(listArgumentCaptor.getValue().size()>0);
     }
 
     @After
