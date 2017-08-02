5944fb3740b236de46063fb331ee91abf36e0350
sachin <sachinshinde7676@gmail.com>
2017-08-03 01:40:53 +0530

    added common code inside RoomParent

--- src/test/java/RocketChatAPI/RocketChatRoomTest/ChatRoomParent/RoomParent.java
+++ src/test/java/RocketChatAPI/RocketChatRoomTest/ChatRoomParent/RoomParent.java
@@ -1,7 +1,45 @@
 package RocketChatAPI.RocketChatRoomTest.ChatRoomParent;
 
+import io.rocketchat.common.data.model.ErrorObject;
+import io.rocketchat.core.RocketChatAPI;
+import io.rocketchat.core.adapter.CoreAdapter;
+import io.rocketchat.core.model.TokenObject;
+import org.junit.After;
+import org.mockito.MockitoAnnotations;
+
 /**
  * Created by sachin on 2/8/17.
  */
-public class RoomParent {
+public class RoomParent extends CoreAdapter{
+
+    private static String serverurl="wss://demo.rocket.chat/websocket";
+
+    String username="testuserrocks";
+    String password="testuserrocks";
+
+    public RocketChatAPI api;
+
+    public void setUpBefore(Boolean connect){
+        MockitoAnnotations.initMocks( this );
+        System.out.println("before got called");
+        api= new RocketChatAPI(serverurl);
+        api.setReconnectionStrategy(null);
+        api.connect(this);
+    }
+
+    @Override
+    public void onConnect(String sessionID) {
+        api.login(username,password,this);
+    }
+
+    @Override
+    public void onLogin(TokenObject token, ErrorObject error) {
+        api.getSubscriptions(this);
+    }
+
+    @After
+    public void logout(){
+        api.logout(null);
+    }
 }
+
