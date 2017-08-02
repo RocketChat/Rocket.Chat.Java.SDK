d83d5a0cecdc811a21018f412e0fd5937b4a0f45
sachin <sachinshinde7676@gmail.com>
2017-08-02 13:38:33 +0530

    added packages for unit testing of Core SDK under test

--- src/main/java/io/rocketchat/core/callback/HistoryListener.java
+++ src/main/java/io/rocketchat/core/callback/HistoryListener.java
@@ -1,7 +1,6 @@
 package io.rocketchat.core.callback;
 
 import io.rocketchat.common.data.model.ErrorObject;
-import io.rocketchat.common.data.model.Message;
 import io.rocketchat.common.listener.Listener;
 import io.rocketchat.core.model.RocketChatMessage;
 
