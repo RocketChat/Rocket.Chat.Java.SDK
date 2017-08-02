c83d2fc2ca2557fa600678087691e9405b133606
sachin <sachinshinde7676@gmail.com>
2017-08-03 01:51:10 +0530

    updated RoomParent to get suitable room

--- src/test/java/RocketChatAPI/RocketChatRoomTest/ChatRoomParent/RoomParent.java
+++ src/test/java/RocketChatAPI/RocketChatRoomTest/ChatRoomParent/RoomParent.java
@@ -3,9 +3,13 @@ package RocketChatAPI.RocketChatRoomTest.ChatRoomParent;
 import io.rocketchat.common.data.model.ErrorObject;
 import io.rocketchat.core.RocketChatAPI;
 import io.rocketchat.core.adapter.CoreAdapter;
+import io.rocketchat.core.factory.ChatRoomFactory;
+import io.rocketchat.core.model.SubscriptionObject;
 import io.rocketchat.core.model.TokenObject;
 import org.junit.After;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
 
 /**
  * Created by sachin on 2/8/17.
@@ -35,6 +39,25 @@ public class RoomParent extends CoreAdapter{
     @Override
     public void onLogin(TokenObject token, ErrorObject error) {
         api.getSubscriptions(this);
+
+    }
+
+    @Override
+    public void onGetSubscriptions(ArrayList<SubscriptionObject> subscriptions, ErrorObject error) {
+        ChatRoomFactory factory=api.getFactory();
+        //Listing number of rooms
+        for (RocketChatAPI.ChatRoom room : factory.getChatRooms()){
+            System.out.println("Room name is "+room.getRoomData().getRoomName());
+            System.out.println("Room type is "+room.getRoomData().getRoomType());
+            System.out.println("Room id is "+room.getRoomData().getRoomId());
+        }
+
+        RocketChatAPI.ChatRoom room=factory.createChatRooms(subscriptions).getChatRoomByName("PUBLICTESTGROUP");
+        onRoom(room);
+    }
+
+    public void onRoom(RocketChatAPI.ChatRoom room){
+
     }
 
     @After
