acd82f442111295652bc2f2095e3b4258abaa31a
sachin <sachinshinde7676@gmail.com>
2017-08-11 01:44:07 +0530

    written subrpc for getting delete message event

--- src/main/java/io/rocketchat/core/rpc/CoreSubRPC.java
+++ src/main/java/io/rocketchat/core/rpc/CoreSubRPC.java
@@ -9,14 +9,21 @@ public class CoreSubRPC extends SubRPC {
     private static final String STREAM_ROOM_MESSAGES = "stream-room-messages";
     private static final String STREAM_NOTIFY_ROOM = "stream-notify-room";
 
+    // Events
+    private static final String TYPING_EVENT = "/typing";
+    private static final String DELETE_EVENT = "/deleteMessage";
+
     public static String subscribeRoomMessageEvent(String uniqueid, String room_id, Boolean persistenceEnable) {
         return getRemoteSubscriptionObject(uniqueid, STREAM_ROOM_MESSAGES, room_id, persistenceEnable).toString();
     }
 
     public static String subscribeRoomTypingEvent(String uniqueid, String room_id, Boolean persistenceEnable) {
-        return getRemoteSubscriptionObject(uniqueid, STREAM_NOTIFY_ROOM, room_id + "/typing", persistenceEnable).toString();
+        return getRemoteSubscriptionObject(uniqueid, STREAM_NOTIFY_ROOM, room_id + TYPING_EVENT, persistenceEnable).toString();
     }
 
+    public static String subscribeRoomMessageDeleteEvent(String uniqueid, String room_id, Boolean persistenceEnable) {
+        return getRemoteSubscriptionObject(uniqueid, STREAM_NOTIFY_ROOM, room_id + DELETE_EVENT, persistenceEnable).toString();
+    }
     public static String unsubscribeRoom(String subId) {
         return getRemoteUnsubscriptionObject(subId).toString();
     }
