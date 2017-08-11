a9f6bd7410ee6302b83d92ca80876d7929396379
sachin <sachinshinde7676@gmail.com>
2017-08-11 12:26:53 +0530

    added connectivity manager to network in common

---
+++ src/main/java/io/rocketchat/common/network/ConnectivityManager.java
@@ -0,0 +1,39 @@
+package io.rocketchat.common.network;
+
+import io.rocketchat.common.listener.ConnectListener;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by sachin on 11/8/17.
+ */
+public class ConnectivityManager {
+
+    ConcurrentLinkedQueue <ConnectListener> listeners;
+
+    public void register(ConnectListener listener) {
+        listeners.add(listener);
+    }
+
+    public void publishConnect (String sessionId) {
+        for (ConnectListener listener : listeners){
+            listener.onConnect(sessionId);
+        }
+    }
+
+    public void publishDisconnect(boolean closedByServer) {
+        for (ConnectListener listener : listeners){
+            listener.onDisconnect(closedByServer);
+        }
+    }
+
+    public void publishConnectError(Exception websocketException) {
+        for (ConnectListener listener : listeners){
+            listener.onConnectError(websocketException);
+        }
+    }
+
+    public Boolean unRegister(ConnectListener listener) {
+        return listeners.remove(listener);
+    }
+}
