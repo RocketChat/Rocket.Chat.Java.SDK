597c6a4201c2cdf028c6b6efd35f0db851ce5ebb
sachin <sachinshinde7676@gmail.com>
2017-08-25 01:31:31 +0530

    added set of interfaces to get proper instance in application class

---
+++ rocketchat-livechat/src/main/java/com/rocketchat/ILiveChatAPI.java
@@ -0,0 +1,10 @@
+package com.rocketchat;
+
+import com.rocketchat.livechat.LiveChatAPI;
+
+/**
+ * Created by sachin on 25/8/17.
+ */
+public interface ILiveChatAPI {
+    LiveChatAPI getLiveChatAPI();
+}
