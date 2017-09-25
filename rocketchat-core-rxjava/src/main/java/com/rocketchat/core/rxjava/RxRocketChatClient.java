8e50388760f5c0a45bdaf3fb196d81dd1e6820e9
Lucio Maciel <luciofm@gmail.com>
2017-09-25 13:04:07 -0300

    Initial RxJava2 module

---
+++ rocketchat-core-rxjava/src/main/java/com/rocketchat/core/rxjava/RxRocketChatClient.java
@@ -0,0 +1,54 @@
+package com.rocketchat.core.rxjava;
+
+import com.rocketchat.common.RocketChatException;
+import com.rocketchat.common.listener.ConnectListener;
+import com.rocketchat.core.RocketChatAPI;
+import com.rocketchat.core.callback.LoginCallback;
+import com.rocketchat.core.model.Token;
+
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
+
+public class RxRocketChatClient implements ConnectListener {
+
+    private final RocketChatAPI client;
+
+    public RxRocketChatClient(RocketChatAPI client) {
+        this.client = client;
+    }
+
+    public Single<Token> signin(final String username, final String password) {
+        return Single.create(new SingleOnSubscribe<Token>() {
+            @Override
+            public void subscribe(final SingleEmitter<Token> emitter) throws Exception {
+                client.signin(username, password, new LoginCallback() {
+                    @Override
+                    public void onLoginSuccess(Token token) {
+                        emitter.onSuccess(token);
+                    }
+
+                    @Override
+                    public void onError(RocketChatException error) {
+                        emitter.onError(error);
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onConnect(String sessionID) {
+
+    }
+
+    @Override
+    public void onDisconnect(boolean closedByServer) {
+
+    }
+
+    @Override
+    public void onConnectError(Throwable websocketException) {
+
+    }
+}
---
+++ rocketchat-core-rxjava/src/test/java/com/rocketchat/core/rxjava/RxRocketChatClientTest.java
@@ -0,0 +1,109 @@
+package com.rocketchat.core.rxjava;
+
+import com.rocketchat.common.RocketChatAuthException;
+import com.rocketchat.common.RocketChatNetworkErrorException;
+import com.rocketchat.core.RocketChatAPI;
+import com.rocketchat.core.callback.LoginCallback;
+import com.rocketchat.core.model.Token;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import io.reactivex.observers.TestObserver;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RxRocketChatClientTest {
+    RxRocketChatClient sut;
+
+    @Mock
+    RocketChatAPI api;
+
+    Token token = new Token("userId", "token", null);
+
+    @Before
+    public void setup() {
+        sut = new RxRocketChatClient(api);
+    }
+
+    @Test
+    public void tesSigninShouldBeSuccessfull() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ((LoginCallback) invocation.getArguments()[2]).onLoginSuccess(token);
+                return null;
+            }
+        }).when(api).signin(any(String.class), any(String.class), any(LoginCallback.class));
+
+        TestObserver<Token> testObserver = sut.signin("username", "password").test();
+
+        testObserver.assertValue(token -> token != null && token.getUserId().contentEquals("userId")
+                && token.getAuthToken().contentEquals("token"));
+        testObserver.assertComplete();
+        testObserver.assertNoErrors();
+    }
+
+    @Test
+    public void testSigninShouldFailWithNullUsername() {
+        doThrow(new IllegalArgumentException("username == null"))
+                .when(api).signin(isNull(), any(String.class), any(LoginCallback.class));
+
+        TestObserver<Token> testObserver = sut.signin(null, "password").test();
+
+        testObserver.assertError(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void testSigninShouldFailWithNullPassword() {
+        doThrow(new IllegalArgumentException("password == null"))
+                .when(api).signin(any(String.class), isNull(), any(LoginCallback.class));
+
+        TestObserver<Token> testObserver = sut.signin("username", null).test();
+
+        testObserver.assertError(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void testSigninShouldFailWithRocketChatNetworkErrorException() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ((LoginCallback) invocation.getArguments()[2]).onError(new RocketChatNetworkErrorException("network error"));
+                return null;
+            }
+        }).when(api).signin(any(String.class), any(String.class), any(LoginCallback.class));
+
+        TestObserver<Token> testObserver = sut.signin("username", "password").test();
+        testObserver.assertError(throwable -> throwable != null
+                && throwable instanceof RocketChatNetworkErrorException
+                && throwable.getMessage().contentEquals("network error"));
+        testObserver.assertNoValues();
+    }
+
+    @Test
+    public void testSigninShouldFailWithRocketChatAuthException() {
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ((LoginCallback) invocation.getArguments()[2]).onError(new RocketChatAuthException("Invalid credentials"));
+                return null;
+            }
+        }).when(api).signin(any(String.class), any(String.class), any(LoginCallback.class));
+
+        TestObserver<Token> testObserver = sut.signin("username", "password").test();
+        testObserver.assertError(throwable -> throwable != null
+                && throwable instanceof RocketChatAuthException
+                && throwable.getMessage().contentEquals("Invalid credentials"));
+        testObserver.assertNoValues();
+    }
+}
