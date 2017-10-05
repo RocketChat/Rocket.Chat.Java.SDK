8b8e1b09c29e2c593e25fc36a15fbd204bedceb6
sachin <sachinshinde7676@gmail.com>
2017-10-06 00:43:51 +0530

    added all files along with jCenter as a repo

--- sample/src/main/java/com/rocketchat/sample/Main.java
+++ sample/src/main/java/com/rocketchat/sample/Main.java
@@ -7,13 +7,12 @@ import com.rocketchat.common.network.ReconnectionStrategy;
 import com.rocketchat.common.utils.Logger;
 import com.rocketchat.core.ChatRoom;
 import com.rocketchat.core.RocketChatClient;
+import com.rocketchat.core.callback.HistoryCallback;
 import com.rocketchat.core.callback.LoginCallback;
-import com.rocketchat.core.callback.MessageCallback;
 import com.rocketchat.core.factory.ChatRoomFactory;
 import com.rocketchat.core.model.Message;
 import com.rocketchat.core.model.Subscription;
 import com.rocketchat.core.model.Token;
-
 import java.util.List;
 
 /**
@@ -25,9 +24,6 @@ public class Main {
     private static String serverurl = "ws://demo.rocket.chat/websocket";
     private static String baseUrl = "https://demo.rocket.chat/";
     RocketChatClient client;
-    ChatRoom room;
-
-    String file_path = "/home/sachin/Pictures/pain.jpg";
 
     public static void main(String[] args) {
         new Main().call();
@@ -48,7 +44,45 @@ public class Main {
     ConnectListener connectListener = new ConnectListener() {
         public void onConnect(String sessionID) {
             System.out.println("Connected to server");
-            client.loginUsingToken("mb3ChOAItBEtQ9x9n30xzU1EmRsmaFRVRoO0GWjdfPQ", loginCallback);
+            client.login("sachin.shinde", "sachin123", new LoginCallback() {
+                @Override
+                public void onLoginSuccess(Token token) {
+                    System.out.println("Login is successful");
+                    client.getSubscriptions(new SimpleListCallback<Subscription>() {
+                        @Override
+                        public void onSuccess(List<Subscription> list) {
+                            ChatRoomFactory factory = client.getChatRoomFactory();
+                            factory.createChatRooms(list);
+                            ChatRoom chatRoom = factory.getChatRoomByName("general");
+                            chatRoom.getChatHistory(20, null, null, new HistoryCallback() {
+                                @Override
+                                public void onLoadHistory(List<Message> list, int unreadNotLoaded) {
+                                    System.out.println("got data here" + list.size());
+                                    for (Message message : list) {
+                                        System.out.println("Message is " + message.message());
+                                    }
+                                }
+
+                                @Override
+                                public void onError(RocketChatException error) {
+
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onError(RocketChatException error) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onError(RocketChatException error) {
+
+                }
+            });
+
         }
 
         public void onConnectError(Throwable websocketException) {
@@ -60,111 +94,6 @@ public class Main {
         }
     };
 
-    public LoginCallback loginCallback = new LoginCallback() {
-        @Override
-        public void onLoginSuccess(Token token) {
-            client.getSubscriptions(subscriptionsCallback);
-        }
-
-        @Override
-        public void onError(RocketChatException error) {
-            System.out.println("Error: " + error);
-        }
-    };
-
-    public SimpleListCallback<Subscription> subscriptionsCallback = new SimpleListCallback<Subscription>() {
-        @Override
-        public void onSuccess(List<Subscription> subscriptions) {
-            ChatRoomFactory factory = client.getChatRoomFactory();
-            room = factory.createChatRooms(subscriptions).getChatRoomByName("general");
-            room.subscribeRoomMessageEvent(null, messageCallback);
-        }
-
-        @Override
-        public void onError(RocketChatException error) {
-            System.out.println("Error: " + error);
-        }
-    };
-
-    public MessageCallback.SubscriptionCallback messageCallback = new MessageCallback.SubscriptionCallback() {
-        public void onMessage(String roomId, Message message) {
-            System.out.println("Got message " + message.message());
-            switch (message.getMsgType()) {
-                case TEXT:
-                    System.out.println("This is a text message");
-                    break;
-                /*case ATTACHMENT:
-                    List<TAttachment> attachments = message.attachments();
-                    for (TAttachment attachment : attachments) {
-                        switch (attachment.getAttachmentType()) {
-                            case TEXT_ATTACHMENT:
-                                System.out.println("This is a reply or quote to a message");
-                                break;
-                            case IMAGE:
-                                System.out.println("There is a image attachment");
-                                break;
-                            case AUDIO:
-                                System.out.println("There is a audio attachment");
-                                break;
-                            case VIDEO:
-                                System.out.println("There is a video attachment");
-                                break;
-                        }
-                    }
-                    break;*/
-                case MESSAGE_EDITED:
-                    System.out.println("Message has been edited");
-                    break;
-                case MESSAGE_STARRED:
-                    System.out.println("Message is starred now");
-                    break;
-                case MESSAGE_REACTION:
-                    System.out.println("Got message reaction");
-                    break;
-                case MESSAGE_REMOVED:
-                    System.out.println("Message is deleted");
-                    break;
-                case ROOM_NAME_CHANGED:
-                    System.out.println("Room name changed");
-                    break;
-                case ROOM_ARCHIVED:
-                    System.out.println("Room is archived");
-                    break;
-                case ROOM_UNARCHIVED:
-                    System.out.println("Room is unarchieved");
-                    break;
-                case USER_ADDED:
-                    System.out.println("User added to the room");
-                    break;
-                case USER_REMOVED:
-                    System.out.println("User removed from the room");
-                    break;
-                case USER_JOINED:
-                    System.out.println("User joined the room");
-                    break;
-                case USER_LEFT:
-                    System.out.println("User left the room");
-                    break;
-                case USER_MUTED:
-                    System.out.println("User muted now");
-                    break;
-                case USER_UNMUTED:
-                    System.out.println("User un-muted now");
-                    break;
-                case WELCOME:
-                    System.out.println("User welcomed");
-                    break;
-                case SUBSCRIPTION_ROLE_ADDED:
-                    System.out.println("Subscription role added");
-                    break;
-                case SUBSCRIPTION_ROLE_REMOVED:
-                    System.out.println("Subscription role removed");
-                    break;
-                case OTHER:
-                    break;
-            }
-        }
-    };
 
     private Logger logger = new Logger() {
         @Override
