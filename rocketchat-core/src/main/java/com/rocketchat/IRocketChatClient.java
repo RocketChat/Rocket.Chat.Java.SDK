32639278944ea7bbea3d05862814bb36a79db1f9
sachin <sachinshinde7676@gmail.com>
2017-10-20 00:28:28 +0530

    removed unnecessary client interface implementation

--- rocketchat-core/src/main/java/com/rocketchat/IRocketChatClient.java
+++
@@ -1,7 +0,0 @@
-package com.rocketchat;
-
-import com.rocketchat.core.RocketChatClient;
-
-public interface IRocketChatClient {
-    RocketChatClient getRocketChatAPI();
-}
--- rocketchat-livechat/src/main/java/com/rocketchat/ILiveChatClient.java
+++
@@ -1,8 +0,0 @@
-package com.rocketchat;
-
-
-import com.rocketchat.livechat.LiveChatClient;
-
-public interface ILiveChatClient {
-    LiveChatClient getLiveChatClient();
-}
