cd4816757940e2f36ba5aac6c958696ae9955dbb
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-10-20 13:58:50 -0200

    Update RestImplTest.java
    Includes the getRoomFiles method tests.

--- rocketchat-core/src/test/java/com/rocketchat/core/RestImplTest.java
+++ rocketchat-core/src/test/java/com/rocketchat/core/RestImplTest.java
@@ -5,10 +5,14 @@ import com.rocketchat.common.RocketChatException;
 import com.rocketchat.common.RocketChatInvalidResponseException;
 import com.rocketchat.common.data.CommonJsonAdapterFactory;
 import com.rocketchat.common.data.TimestampAdapter;
+import com.rocketchat.common.data.model.BaseRoom;
 import com.rocketchat.common.utils.NoopLogger;
+import com.rocketchat.common.utils.Sort;
 import com.rocketchat.core.callback.LoginCallback;
+import com.rocketchat.core.callback.RoomCallback;
 import com.rocketchat.core.model.JsonAdapterFactory;
 import com.rocketchat.core.model.Token;
+import com.rocketchat.core.model.attachment.Attachment;
 import com.rocketchat.core.provider.TokenProvider;
 import com.squareup.moshi.Moshi;
 
@@ -21,6 +25,8 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
 import io.fabric8.mockwebserver.DefaultMockServer;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -29,6 +35,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
@@ -36,31 +43,23 @@ import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class RestImplTest {
 
-    RestImpl sut;
-    OkHttpClient client;
-    HttpUrl baseUrl;
-
-    @Mock
-    TokenProvider tokenProvider;
-
-    @Mock
-    LoginCallback loginCallback;
-
-    @Captor
-    ArgumentCaptor<Token> tokenCaptor;
-
-    @Captor
-    ArgumentCaptor<RocketChatException> exceptionCaptor;
-
-    DefaultMockServer mockServer;
+    private RestImpl rest;
+    private DefaultMockServer mockServer;
+    @Mock private TokenProvider tokenProvider;
+    @Mock private LoginCallback loginCallback;
+    @Mock private RoomCallback.GetFilesCallback getFilesCallback;
+    @Captor private ArgumentCaptor<Token> tokenCaptor;
+    @Captor private ArgumentCaptor<Integer> totalCaptor;
+    @Captor private ArgumentCaptor<List<Attachment>> listAttachmentCaptor;
+    @Captor private ArgumentCaptor<RocketChatException> exceptionCaptor;
 
     @Before
     public void setup() {
         mockServer = new DefaultMockServer();
         mockServer.start();
 
-        baseUrl = HttpUrl.parse(mockServer.url("/"));
-        client = new OkHttpClient();
+        HttpUrl baseUrl = HttpUrl.parse(mockServer.url("/"));
+        OkHttpClient client = new OkHttpClient();
 
         Moshi moshi = new Moshi.Builder()
                 .add(new TimestampAdapter())
@@ -68,18 +67,46 @@ public class RestImplTest {
                 .add(CommonJsonAdapterFactory.create())
                 .build();
 
-        sut = new RestImpl(client, moshi, baseUrl, tokenProvider, new NoopLogger());
+        rest = new RestImpl(client, moshi, baseUrl, tokenProvider, new NoopLogger());
     }
 
-    // start signin tests
+    //     _____ _____ _____ _   _ _____ _   _    _______ ______  _____ _______ _____
+    //    / ____|_   _/ ____| \ | |_   _| \ | |  |__   __|  ____|/ ____|__   __/ ____|
+    //   | (___   | || |  __|  \| | | | |  \| |     | |  | |__  | (___    | | | (___
+    //    \___ \  | || | |_ | . ` | | | | . ` |     | |  |  __|  \___ \   | |  \___ \
+    //    ____) |_| || |__| | |\  |_| |_| |\  |     | |  | |____ ____) |  | |  ____) |
+    //   |_____/|_____\_____|_| \_|_____|_| \_|     |_|  |______|_____/   |_| |_____/
+    //
+    //
+
+    @Test(expected = NullPointerException.class)
+    public void testSigninShouldFailWithNullUsername() {
+        rest.signin(null, "password", loginCallback);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSigninShouldFailWithNullPassword() {
+        rest.signin("username", null, loginCallback);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSigninShouldFailWithNullCallback() {
+        rest.signin("username", "password", null);
+    }
+
     @Test
-    public void tesSigninShouldBeSuccessfull() {
-        mockServer.expect().post().withPath("/api/v1/login")
-                .andReturn(200, "{\"status\": \"success\",\"data\": {\"authToken\": \"token\",\"userId\": \"userid\"}}").once();
+    public void tesSigninShouldBeSuccessful() {
+        mockServer.expect()
+                .post()
+                .withPath("/api/v1/login")
+                .andReturn(200, "{\"status\": \"success\",\"data\": {\"authToken\": \"token\",\"userId\": \"userid\"}}")
+                .once();
 
-        sut.signin("user", "password", loginCallback);
+        rest.signin("user", "password", loginCallback);
 
-        verify(loginCallback, timeout(100).only()).onLoginSuccess(tokenCaptor.capture());
+        verify(loginCallback, timeout(100).only())
+                .onLoginSuccess(tokenCaptor.capture());
+
         Token token = tokenCaptor.getValue();
         assertThat(token.getUserId(), is(equalTo("userid")));
         assertThat(token.getAuthToken(), is(equalTo("token")));
@@ -88,11 +115,16 @@ public class RestImplTest {
 
     @Test
     public void testSigninShouldFailOnInvalidJson() {
-        mockServer.expect().post().withPath("/api/v1/login")
-                .andReturn(200, "NOT A JSON").once();
+        mockServer.expect()
+                .post()
+                .withPath("/api/v1/login")
+                .andReturn(200, "NOT A JSON")
+                .once();
 
-        sut.signin("user", "password", loginCallback);
-        verify(loginCallback, timeout(100).only()).onError(exceptionCaptor.capture());
+        rest.signin("user", "password", loginCallback);
+        verify(loginCallback, timeout(100).only())
+                .onError(exceptionCaptor.capture());
+
         RocketChatException exception = exceptionCaptor.getValue();
         assertThat(exception, is(instanceOf(RocketChatInvalidResponseException.class)));
         assertThat(exception.getMessage(), is(equalTo("A JSONObject text must begin with '{' at character 1")));
@@ -100,12 +132,17 @@ public class RestImplTest {
     }
 
     @Test
-    public void testSiginShouldFailWithAuthExceptionOn401() {
-        mockServer.expect().post().withPath("/api/v1/login")
-                .andReturn(401, "{\"status\": \"error\",\"message\": \"Unauthorized\"}").once();
+    public void testSigninShouldFailWithAuthExceptionOn401() {
+        mockServer.expect()
+                .post()
+                .withPath("/api/v1/login")
+                .andReturn(401, "{\"status\": \"error\",\"message\": \"Unauthorized\"}")
+                .once();
 
-        sut.signin("user", "password", loginCallback);
-        verify(loginCallback, timeout(200).only()).onError(exceptionCaptor.capture());
+        rest.signin("user", "password", loginCallback);
+
+        verify(loginCallback, timeout(200).only())
+                .onError(exceptionCaptor.capture());
         RocketChatException exception = exceptionCaptor.getValue();
         assertThat(exception, is(instanceOf(RocketChatAuthException.class)));
         assertThat(exception.getMessage(), is(equalTo("Unauthorized")));
@@ -113,26 +150,88 @@ public class RestImplTest {
 
     @Test
     public void testSigninShouldFailIfNot2xx() {
-        sut.signin("user", "password", loginCallback);
-        verify(loginCallback, timeout(200).only()).onError(exceptionCaptor.capture());
+        rest.signin("user", "password", loginCallback);
+
+        verify(loginCallback, timeout(200).only())
+                .onError(exceptionCaptor.capture());
         RocketChatException exception = exceptionCaptor.getValue();
         assertThat(exception, is(instanceOf(RocketChatException.class)));
 
     }
 
+    //     _____ ______ _______     _____   ____   ____  __  __        ______ _____ _      ______  _____   _______ ______  _____ _______ _____
+    //    / ____|  ____|__   __|   |  __ \ / __ \ / __ \|  \/  |      |  ____|_   _| |    |  ____|/ ____| |__   __|  ____|/ ____|__   __/ ____|
+    //   | |  __| |__     | |______| |__) | |  | | |  | | \  / |______| |__    | | | |    | |__  | (___      | |  | |__  | (___    | | | (___
+    //   | | |_ |  __|    | |______|  _  /| |  | | |  | | |\/| |______|  __|   | | | |    |  __|  \___ \     | |  |  __|  \___ \   | |  \___ \
+    //   | |__| | |____   | |      | | \ \| |__| | |__| | |  | |      | |     _| |_| |____| |____ ____) |    | |  | |____ ____) |  | |  ____) |
+    //    \_____|______|  |_|      |_|  \_\\____/ \____/|_|  |_|      |_|    |_____|______|______|_____/     |_|  |______|_____/   |_| |_____/
+    //
+    //
+
     @Test(expected = NullPointerException.class)
-    public void testSigninShouldFailWithNullUsername() {
-        sut.signin(null, "password", loginCallback);
+    public void testGetRoomFilesShouldFailWithNullRoomId() {
+        rest.getRoomFiles(null, BaseRoom.RoomType.PUBLIC, "0", Attachment.SortBy.UPLOADED_DATE, Sort.DESC, getFilesCallback);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testSigninShouldFailWithNullPassword() {
-        sut.signin("username", null, loginCallback);
+    public void testGetRoomFilesShouldFailWithNullRoomType() {
+        rest.getRoomFiles("roomId", null, "0", Attachment.SortBy.UPLOADED_DATE, Sort.DESC, getFilesCallback);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testSigninShouldFailWithNullCallback() {
-        sut.signin("username", "password", null);
+    public void testGetRoomFilesShouldFailWithNullOffset() {
+        rest.getRoomFiles("roomId", BaseRoom.RoomType.PUBLIC, null, Attachment.SortBy.UPLOADED_DATE, Sort.DESC, getFilesCallback);
     }
-    // end signin tests
+
+    @Test(expected = NullPointerException.class)
+    public void testGetRoomFilesShouldFailWithNullSortBy() {
+        rest.getRoomFiles("roomId", BaseRoom.RoomType.PUBLIC, "0", null, Sort.DESC, getFilesCallback);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testGetRoomFilesShouldFailWithNullSort() {
+        rest.getRoomFiles("roomId", BaseRoom.RoomType.PUBLIC, "0", Attachment.SortBy.UPLOADED_DATE, null, getFilesCallback);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testGetRoomFilesShouldFailWithNullCallback() {
+        rest.getRoomFiles("roomId", BaseRoom.RoomType.PUBLIC, "0", Attachment.SortBy.UPLOADED_DATE, Sort.DESC, null);
+    }
+
+    @Test
+    public void testGetRoomFilesShouldFailOnInvalidJson() {
+        mockServer.expect()
+                .get()
+                .withPath("/api/v1/channels.files")
+                .andReturn(200, "NOT A JSON")
+                .once();
+
+        rest.getRoomFiles("general", BaseRoom.RoomType.PUBLIC, "0", Attachment.SortBy.UPLOADED_DATE, Sort.DESC, getFilesCallback);
+        verify(getFilesCallback, timeout(100).only())
+                .onError(exceptionCaptor.capture());
+
+        RocketChatException exception = exceptionCaptor.getValue();
+        assertThat(exception.getMessage(), is(equalTo("A JSONObject text must begin with '{' at character 0")));
+        assertThat(exception.getCause(), is(instanceOf(JSONException.class)));
+    }
+
+    //todo this is failing
+    @Test
+    public void testGetRoomFilesShouldBeSuccessful() {
+        mockServer.expect()
+                .get()
+                .withPath("api/v1/channels.files")
+                .andReturn(200, "{\"status\": \"success\"}")
+                .once();
+
+        rest.getRoomFiles("general", BaseRoom.RoomType.PUBLIC, "0", Attachment.SortBy.UPLOADED_DATE, Sort.DESC, getFilesCallback);
+
+        verify(getFilesCallback, timeout(100).only())
+                .onGetRoomFiles(totalCaptor.capture(), listAttachmentCaptor.capture());
+
+        int total = totalCaptor.getValue();
+        List<Attachment> attachmentList = listAttachmentCaptor.capture();
+        assertThat(total, is(notNullValue()));
+        assertThat(attachmentList, is(notNullValue()));
+    }
 }
